0553f4a115e8bcdabedbcf816dd51443b93e20e9
Alberto <albertoadent@gmail.com>
2024-10-04 03:09:37 -0400

    reordered table drop

--- application/seeds/__init__.py
+++ application/seeds/__init__.py
@@ -1,34 +1,35 @@
 from flask.cli import AppGroup
 from application.models.db import db, environment, SCHEMA
 from .admins import seed_admins, undo_admins
-from .workshop_types import seed_workshop_types,undo_workshop_types
-from .locations import seed_locations,undo_locations
-from .clients import seed_clients,undo_clients
+from .workshop_types import seed_workshop_types, undo_workshop_types
+from .locations import seed_locations, undo_locations
+from .clients import seed_clients, undo_clients
 from .workshops import seed_workshops, undo_workshops
 from .templates import seed_templates, undo_templates
 from .pages import seed_pages, undo_pages
-from .content import seed_contents,undo_contents
+from .content import seed_contents, undo_contents
+
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
-seed_commands = AppGroup('seed')
+seed_commands = AppGroup("seed")
 
 
 # Creates the `flask seed all` command
-@seed_commands.command('all')
+@seed_commands.command("all")
 def seed():
-    if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+    if environment == "production":
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_admins()
-        undo_workshop_types()
-        undo_locations()
-        undo_clients()
-        undo_workshops()
-        undo_templates()
-        undo_pages()
         undo_contents()
+        undo_pages()
+        undo_templates()
+        undo_workshops()
+        undo_clients()
+        undo_locations()
+        undo_workshop_types()
+        undo_admins()
     seed_admins()
     seed_workshop_types()
     seed_locations()
@@ -41,14 +42,14 @@ def seed():
 
 
 # Creates the `flask seed undo` command
-@seed_commands.command('undo')
+@seed_commands.command("undo")
 def undo():
-    undo_admins()
-    undo_workshop_types()
-    undo_locations()
-    undo_clients()
-    undo_workshops()
-    undo_templates()
-    undo_pages()
     undo_contents()
+    undo_pages()
+    undo_templates()
+    undo_workshops()
+    undo_clients()
+    undo_locations()
+    undo_workshop_types()
+    undo_admins()
     # Add other undo functions here
