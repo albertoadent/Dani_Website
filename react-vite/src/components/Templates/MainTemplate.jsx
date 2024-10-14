cea365d46a0471f61bd3c65c8937c7dcd255abd9
Alberto <albertoadent@gmail.com>
2024-10-14 11:06:14 -0400

    final

--- react-vite/src/components/Templates/MainTemplate.jsx
+++ react-vite/src/components/Templates/MainTemplate.jsx
@@ -3,12 +3,12 @@
 import MainCard from "../Cards/MainCard";
 
 export default function MainTemplate({ page }) {
-  
   return (
     <div className="grid grid-cols-2 gap-y-10 gap-x-2 p-10">
       {page?.content &&
-        page.content.map(
-          ({ header, subHeader, text, imageUrl, linkTo }, index) => (
+        page.content
+          .sort((a, b) => Number(a.id.split("_")[1]) - Number(b.id.split("_")[1]))
+          .map(({ header, subHeader, text, imageUrl, linkTo }, index) => (
             <div
               key={index}
               className="flex justify-center p-2 h-80 overflow-hidden rounded"
@@ -22,8 +22,7 @@ export default function MainTemplate({ page }) {
                 linkTo={linkTo}
               />
             </div>
-          )
-        )}
+          ))}
     </div>
   );
 }
